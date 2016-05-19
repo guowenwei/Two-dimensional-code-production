8a7071654576d3c86e4a5c72e7dd6d91d3d65d84
魏郭文 <18395069616@163.com>
2016-05-19 09:07:38 +0800

    Initial Commit

---
+++ 二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  二维码
+//
+//  Created by 魏郭文 on 16/5/19.
+//  Copyright © 2016年 魏郭文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 二维码/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  二维码
+//
+//  Created by 魏郭文 on 16/5/19.
+//  Copyright © 2016年 魏郭文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
